2cf01b039062cd67f1c6eb3f024b228687514ecb
andrealuquin <andrealuquin15@gmail.com>
2026-04-27 21:01:57 +0000

    update setup.py and created launch file

---
+++ launch/project_launch.py
@@ -0,0 +1,13 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='robot_tracking_project',
+            executable='controller_node',
+            name='controller_node',
+            output='screen'
+        )
+    ])
--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'robot_tracking_project'
 
@@ -7,16 +9,33 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name],
+        ),
+        (
+            'share/' + package_name,
+            ['package.xml'],
+        ),
+        (
+            os.path.join('share', package_name, 'launch'),
+            glob('launch/*.launch.py'),
+        ),
+        (
+            os.path.join('share', package_name, 'urdf'),
+            glob('urdf/*'),
+        ),
+        (
+            os.path.join('share', package_name, 'config'),
+            glob('config/*'),
+        ),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='andrea',
     maintainer_email='andrealuquin15@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='ROS 2 project for 3D robot arm tracking with Jacobian-based control',
+    license='MIT',
     extras_require={
         'test': [
             'pytest',
@@ -24,6 +43,8 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'controller_node = robot_tracking_project.controller_node:main',
+            'target_publisher = robot_tracking_project.target_publisher:main',
         ],
     },
 )
